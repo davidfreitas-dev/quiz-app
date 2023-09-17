4b23376d7266ed5d852e45bf70e6786d6a313151
David Freitas <davidfreitas@iMac-Pro-de-David.local>
2023-09-17 15:26:46 -0300

    quiz screen criation

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../pages/Home.vue')
+    },
+    {
+      path: '/quiz',
+      name: 'quiz',
+      component: () => import('../pages/Quiz.vue')
+    },
+  ]
+});
+
+export default router;
