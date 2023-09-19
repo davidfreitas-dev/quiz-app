a6650627ea2424d0709771655df195da45c51259
David Freitas <davidfreitas@iMac-Pro-de-David.local>
2023-09-19 10:02:02 -0300

    button component adjustment

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -21,7 +21,7 @@ const props = defineProps({
       'w-full': size === 'block',
       'bg-primary': color === 'primary'
     }"
-    class="inline-flex justify-center items-center text-white text-sm text-center font-semibold border-none rounded-xl transition-all duration-200 ease-in active:scale-95 active:opacity-80 cursor-pointer disabled:cursor-not-allowed disabled:bg-gray-400 select-none py-3 px-6 h-12"
+    class="flex justify-center items-center text-white text-sm text-center font-semibold border-none rounded-xl transition-all duration-200 ease-in active:scale-95 active:opacity-80 cursor-pointer disabled:cursor-not-allowed disabled:bg-gray-400 select-none py-3 px-6 h-12"
   >
     {{ text }}
   </button>
