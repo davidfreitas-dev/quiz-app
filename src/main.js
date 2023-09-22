ec1f2a38f0148aca316b5009f23e94d6637cef8a
David Freitas <davidfreitas@iMac-Pro-de-David.local>
2023-09-22 12:02:26 -0300

    remove pinia import on main

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import { createPinia } from 'pinia';
+
 import App from './App.vue';
 import router from './router';
 
@@ -7,9 +7,7 @@ import './services/firebase-firestore';
 
 import './style.css';
 
-const pinia = createPinia();
 const app = createApp(App);
 
 app.use(router);
-app.use(pinia);
 app.mount('#app');
