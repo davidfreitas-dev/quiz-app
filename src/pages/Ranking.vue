5943ec43addd2a6fc3b90f1aed9bf84c27783e8f
David Freitas <davidfreitas.dev@outlook.com>
2025-04-13 14:46:19 -0300

    add ajuste na listagem do ranking

--- src/pages/Ranking.vue
+++ src/pages/Ranking.vue
@@ -2,12 +2,13 @@
 import { computed, onMounted } from 'vue';
 import { storeToRefs } from 'pinia';
 import { useQuizStore } from '@/stores/quiz';
-import Container from '@/components/Container.vue';
-import Heading from '@/components/Heading.vue';
-import BackButton from '@/components/BackButton.vue';
+import NavBar from '@/components/NavBar.vue';
+import Podium from '@/components/Podium.vue';
 import Avatar from '@/components/Avatar.vue';
+import Container from '@/components/Container.vue';
 
 const quizStore = useQuizStore();
+
 const { usersResults, isLoading } = storeToRefs(quizStore);
 
 const topThree = computed(() => {
@@ -42,101 +43,27 @@ onMounted(async () => {
 
 <template>
   <div class="flex flex-col h-screen">
-    <Container class="bg-primary text-white pb-0 mb-5">
-      <div class="relative flex items-center justify-center h-12">
-        <BackButton route="/" class="absolute left-0" />
-        <Heading
-          size="lg"
-          text="Ranking"
-          class="text-white"
-        />
-      </div>
-  
-      <div v-if="topThree.length === 3" class="w-full flex justify-center items-end pt-3">
-        <!-- 2º lugar -->
-        <div class="flex flex-col items-center w-full">
-          <img
-            :src="topThree[1].image"
-            class="w-16 h-16 rounded-full border-4 border-white shadow-md z-10"
-            alt="Segundo colocado"
-          >
-          <p class="text-sm mt-2 font-semibold text-center">
-            {{ splitUserName(topThree[1].name) }}
-          </p>
-          <div class="text-xs my-1 px-4 py-1.5 rounded-lg bg-white text-primary font-bold">
-            {{ topThree[1].score }} XP
-          </div>
-          <div class="bg-[#aea7ed] text-white px-3 py-1 mt-2 font-medium w-full text-center h-[70px]">
-            <div class="flex flex-col justify-center h-full">
-              <span class="text-3xl font-bold">2</span>
+    <NavBar route="/" class="bg-primary" />
+    <Podium :top-three="topThree" :split-user-name="splitUserName" />
+    <Container class="flex-1 overflow-y-auto bg-light">
+      <div
+        v-for="(user, index) in others"
+        :key="user.id"
+        class="flex items-center justify-between bg-white rounded-xl shadow-lg py-4 px-5 mb-3"
+      >
+        <div class="flex items-center gap-5">
+          <span class="font-medium text-gray-500">{{ index + 4 }}</span>
+          <Avatar :image="user?.image" class="w-10 h-10" />
+          <div class="">
+            <div class="text-sm font-semibold leading-6">
+              {{ user.name }}
+            </div>
+            <div class="text-xs text-gray-400">
+              {{ user.score }} pontos
             </div>
           </div>
         </div>
-
-        <!-- 1º lugar -->
-        <div class="flex flex-col items-center w-full">
-          <img
-            :src="topThree[0].image"
-            class="w-20 h-20 rounded-full border-4 border-yellow-400 shadow-md z-10"
-            alt="Primeiro colocado"
-          >
-          <p class="text-sm mt-2 font-bold text-center">
-            {{ splitUserName(topThree[0].name) }}
-          </p>
-          <div class="text-xs my-1 px-4 py-1.5 rounded-lg bg-white text-primary font-bold">
-            {{ topThree[0].score }} XP
-          </div>
-          <div class="bg-[#9a90e6] text-white px-3 py-1 mt-2 font-semibold w-full text-center h-[90px]">
-            <div class="flex flex-col justify-center h-full">
-              <span class="text-3xl font-bold">1</span>
-            </div>
-          </div>
-        </div>
-
-        <!-- 3º lugar -->
-        <div class="flex flex-col items-center w-full">
-          <img
-            :src="topThree[2].image"
-            class="w-16 h-16 rounded-full border-4 border-white shadow-md z-10"
-            alt="Terceiro colocado"
-          >
-          <p class="text-sm mt-2 font-semibold text-center">
-            {{ splitUserName(topThree[2].name) }}
-          </p>
-          <div class="text-xs my-1 px-4 py-1.5 rounded-lg bg-white text-primary font-bold">
-            {{ topThree[2].score }} XP
-          </div>
-          <div class="bg-[#aea7ed] text-white px-3 py-1 mt-2 font-medium w-full text-center h-[55px]">
-            <div class="flex flex-col justify-center h-full">
-              <span class="text-3xl font-bold">3</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </Container>
-  
-    <Container class="flex-1 overflow-y-auto px-5">
-      <!-- RANKING COMPLETO -->
-      <div class="ranking w-full space-y-3 mb-5">
-        <div
-          v-for="(user, index) in others"
-          :key="user.id"
-          class="flex items-center justify-between bg-white rounded-xl shadow p-4"
-        >
-          <div class="flex items-center gap-3">
-            <span class="font-bold">{{ index + 4 }}</span>
-            <Avatar :image="user?.image" class="w-8 h-8" />
-            <div>
-              <div class="text-sm font-semibold">
-                {{ user.name }}
-              </div>
-              <div class="text-xs text-gray-400">
-                {{ user.email }}
-              </div>
-            </div>
-          </div>
-          <span class="font-bold text-sm">{{ user.score }} pts</span>
-        </div>
+        <!-- <span class="font-bold text-sm">{{ user.score }} pts</span> -->
       </div>
     </Container>
   </div>
