9e1b75bbf325667143f9642110c5631c32a0a96a
David Freitas <davidfreitas.dev@outlook.com>
2025-04-18 13:41:00 -0300

    add melhorias no carrgamento de dados da pagina de perfil

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,11 +1,14 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 import { storeToRefs } from 'pinia';
 import { useUserStore } from '@/stores/user';
 import { useAuth } from '@/use/useAuth';
+import { useToast } from '@/use/useToast';
 import { ExclamationTriangleIcon, ArrowRightOnRectangleIcon } from '@heroicons/vue/24/outline';
 
+// UI Components
+import PageLoader from '@/components/PageLoader.vue';
 import Container from '@/components/Container.vue';
 import Avatar from '@/components/Avatar.vue';
 import BackButton from '@/components/BackButton.vue';
@@ -13,12 +16,23 @@ import Heading from '@/components/Heading.vue';
 import UserStats from '@/components/UserStats.vue';
 import Button from '@/components/Button.vue';
 import Modal from '@/components/Modal.vue';
+import Toast from '@/components/Toast.vue';
 
-const router = useRouter();
+const { toast, toastData, handleToast } = useToast();
 
-const { logOut } = useAuth();
+const isLoading = ref(true);
 
-const { user } = storeToRefs(useUserStore()); 
+const withLoading = async (fn) => {
+  isLoading.value = true;
+  try {
+    await fn();
+  } catch (err) {
+    console.error('Erro na requisição: ', err);
+    handleToast('error', 'Ocorreu um erro ao carregar os dados. Tente novamente mais tarde.');
+  } finally {
+    isLoading.value = false;
+  }
+};
 
 const modalRef = ref(null);
 
@@ -30,15 +44,34 @@ const closeModal = () => {
   modalRef.value?.closeModal();
 };
 
+const router = useRouter();
+
+const { logOut } = useAuth();
+
 const signOut = async () => {
   const response = await logOut();
   if (response.status === 'success') {
     router.push('/signin');
   }
 };
+
+const userStore = useUserStore();
+
+const { user } = storeToRefs(useUserStore()); 
+
+const loadData = async () => {
+  await withLoading(async () => {
+    await userStore.fetchUser();
+    console.log(user.value);
+  });
+};
+
+onMounted(loadData);
 </script>
 
 <template>
+  <PageLoader :visible="isLoading" />
+    
   <div class="relative w-full">
     <div class="bg-primary text-white p-5 h-[146px] flex justify-between items-start">
       <div class="flex items-center justify-between w-full">
@@ -55,11 +88,11 @@ const signOut = async () => {
     <div class="absolute left-1/2 -bottom-[180px] transform -translate-x-1/2 w-[90%]">
       <div class="relative flex flex-col items-center gap-3 bg-white shadow-lg p-5 rounded-xl pt-12">
         <div class="absolute -top-12 left-1/2 transform -translate-x-1/2">
-          <Avatar :image="user.image" class="w-20 h-20 rounded-full" />
+          <Avatar :image="user?.image" class="w-20 h-20 rounded-full" />
         </div>
         <div class="flex flex-col items-center">
-          <span class="font-semibold text-lg">{{ user.name }}</span>
-          <span class="text-sm text-secondary">{{ user.email }}</span>
+          <span class="font-semibold text-lg">{{ user?.name }}</span>
+          <span class="text-sm text-secondary">{{ user?.email }}</span>
         </div>
         <UserStats
           :quizzes="2"
@@ -83,4 +116,6 @@ const signOut = async () => {
     </template>  
     Deseja realmente sair e finalizar a sessão?
   </Modal>
+
+  <Toast ref="toast" :toast-data="toastData" />
 </template>
